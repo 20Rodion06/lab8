46346a8e1ac12c03eda54afb9163d0898aa515b7
Rodion <rodionpon06@gmail.com>
2024-12-01 21:55:42 +0300

    Initial commit

---
+++ src/etu/java/lab8/EmptyFilmNameExcept.java
@@ -0,0 +1,7 @@
+package etu.java.lab8;
+
+public class EmptyFilmNameExcept extends Exception {
+    public EmptyFilmNameExcept(String message) {
+        super(message);
+    }
+}
---
+++ src/etu/java/lab8/NoSelectExcept.java
@@ -0,0 +1,7 @@
+package etu.java.lab8;
+
+public class NoSelectExcept extends Exception {
+    public NoSelectExcept(String message) {
+        super(message);
+    }
+}
